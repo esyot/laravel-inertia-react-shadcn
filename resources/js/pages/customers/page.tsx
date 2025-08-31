264f75a94d67a9a36c140c5392644c5a8a99c4bb
VnceMari <vinsmar8888@gmail.com>
2025-08-31 13:49:49 +0800

    refactor(manage-customers): used sectionheader

--- resources/js/pages/customers/page.tsx
+++ resources/js/pages/customers/page.tsx
@@ -1,16 +1,17 @@
 import Layout from "../layout";
 import SectionContent from "../components/section-content";
-import { router } from "@inertiajs/react";
+import SectionHeader from "../components/section-header";
+import { Button } from "@/components/ui/button";
 import { CustomerTable } from "./components/customer-list";
 
 export default function CustomersPage({ customers }: any) {
     return (
         <Layout>
+            <SectionHeader className="flex justify-between items-center ">
+                <h1 className="text-xl font-semibold">Customer List</h1>
+                <Button className="cursor-pointer">Add Customer</Button>
+            </SectionHeader>
             <SectionContent header={false}>
-                <div className="flex justify-between items-center mb-4">
-                    <h2 className="text-lg font-semibold">Customer List</h2>
-                </div>
-
                 <CustomerTable customers={customers} />
             </SectionContent>
         </Layout>
