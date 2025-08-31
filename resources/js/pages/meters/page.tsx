fab4dd2026a090b0736366332aaae7874bc79b06
esyot <rei.nhard3367@gmail.com>
2025-08-31 13:27:24 +0800

    fix(meters): added section header

--- resources/js/pages/meters/page.tsx
+++ resources/js/pages/meters/page.tsx
@@ -3,6 +3,7 @@ import Layout from "../layout";
 import SectionContent from "../components/section-content";
 import { router, usePage } from "@inertiajs/react";
 import { AddMeterReadingDialog } from "./components/meterDialog";
+import SectionHeader from "../components/section-header";
 
 export default function Index({ readings }: any) {
     const { props }: any = usePage();
@@ -14,13 +15,11 @@ export default function Index({ readings }: any) {
     return (
         <main>
             <Layout>
+                <SectionHeader className="flex justify-between">
+                    <h1 className="text-xl font-semibold">Meter Readings</h1>
+                    <AddMeterReadingDialog />
+                </SectionHeader>
                 <SectionContent header={false}>
-                    <div className="flex justify-between">
-                        <h1 className="text-2xl font-bold mb-4">
-                            Meter Readings
-                        </h1>
-                        <AddMeterReadingDialog />
-                    </div>
                     <MeterTable readings={readings} onDelete={handleDelete} />
                 </SectionContent>
             </Layout>
