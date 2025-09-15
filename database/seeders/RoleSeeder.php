dd7953d7b3fe3845d145e92299038a6e7ccff00d
esyot <rei.nhard3367@gmail.com>
2025-09-15 22:45:55 +0800

    fix(ci): deleted ci

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -14,6 +14,7 @@ class RoleSeeder extends Seeder
     public function run(): void
     {
         Role::firstOrCreate(['name' => 'user', 'guard_name' => 'web']);
+        Role::firstOrCreate(['name' => 'customer', 'guard_name' => 'web']);
         Role::firstOrCreate(['name' => 'supervisor', 'guard_name' => 'web']);
         Role::firstOrCreate(['name' => 'lineman', 'guard_name' => 'web']);
         Role::firstOrCreate(['name' => 'meter_reader', 'guard_name' => 'web']);
